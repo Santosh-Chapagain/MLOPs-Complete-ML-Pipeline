b5ab364c4d06e0133e71380f2bacdd140be4b8e2
Santosh Chapgain <santoahsharam98@gmail.com>
2026-06-15 19:57:42 +0545

    Adding param file with different parameters

--- src/data_ingesting.py
+++ src/data_ingesting.py
@@ -3,6 +3,7 @@ import os
 import pandas as pd 
 import numpy as np 
 from sklearn.model_selection import train_test_split
+import yaml 
 
 log_dir= '../logs'
 os.makedirs(log_dir ,exist_ok= True)
@@ -24,6 +25,22 @@ file_handler.setFormatter(formatter)
 
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
+
+def load_params(params_path: str) -> dict:
+    try:
+        with open(params_path , 'r') as file: 
+            params= yaml.safe_load(file)
+        logger.debug("Parameters retrieved from %s", params_path)
+        return params 
+    except FileNotFoundError:
+        logger.error("File not found: %s", params_path)
+        raise 
+    except yaml.YAMLError as e:
+        logger.error("YAML error: %s",e)
+        raise 
+    except Exception as e:
+        logger.error("Unexpected error: %s", e)
+        raise 
 
 
 def load_data(data_url: str) -> pd.DataFrame:
@@ -72,11 +89,13 @@ def save_data(train_data: pd.DataFrame, test_data:pd.DataFrame, data_path: str) 
 
 def main():
     try:
+        params= load_params(params_path= './params.yaml')
+        test_size= params['data_ingesting']['test_size']
         data_path = "https://raw.githubusercontent.com/Santosh-Chapagain/MLOPs-Complete-ML-Pipeline/refs/heads/main/notebook/spam.csv"
         df = load_data(data_url=data_path)
         final_df = preprocess_data(df)
         train_data, test_data = train_test_split(
-            final_df, test_size=0.2, random_state=2)
+            final_df, test_size=test_size, random_state=2)
         save_data(train_data, test_data, data_path='./data')
     except Exception as e:
         logger.error('Failed to complete the data ingestion process: %s', e)
--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -28,6 +28,22 @@ logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
 
+def load_params(params_path: str) -> dict:
+    try: 
+        with open(params_path, 'r') as file:
+            params= yaml.safe_load(file)
+            logger.debug("Parameters retrieved from %s", params_path)
+            return params 
+    except FileNotFoundError:
+        logger.error("File not found: %s", params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error("YAML error: %s", e)
+        raise
+    except Exception as e:
+        logger.error("Unexpected error: %s", e)
+        raise
+
 def load_data(file_path: str) -> pd.DataFrame:
     """Load data from a CSV file."""
     try:
@@ -82,7 +98,8 @@ def save_data(df: pd.DataFrame, file_path: str) -> None:
 
 def main():
     try:
-        max_features = 50
+        params= load_params(params_path= './params.yaml')
+        max_features = params['feature_engineering']['max_feature']
 
         train_data = load_data('./data/processed/train_processed.csv')
         test_data = load_data('./data/processed/test_processed.csv')
